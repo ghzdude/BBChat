c3cf2628ba61f804209555bd0c37cdbfb10baca2
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2023-03-13 23:31:17 -0700

    attempt to move plugins from nh script to main buildscript

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ subprojects {
                 includeGroup("net.dv8tion")
                 includeGroup("net.sf.trove4j")
                 includeGroup("org.apache")
+                includeGroup("org.ajoberstar.grgit")
                 includeGroup("org.apache.commons")
                 includeGroup("org.apache.logging")
                 includeGroup("org.apache.logging.log4j")
@@ -32,4 +33,18 @@ subprojects {
             }
         }
     }
+}
+
+plugins {
+    // idk wtf im doing
+    id("org.jetbrains.gradle.plugin.idea-ext").version("1.1.7")
+    id("org.ajoberstar.grgit").version("4.1.1")
+    id("com.palantir.git-version").version("0.13.0")
+    id("com.diffplug.spotless").version("6.7.2")
+    id("com.modrinth.minotaur").version("2.+")
+    // id 'com.modrinth.minotaur' version '2.+' apply false
+    id("com.matthewprenger.cursegradle").version("1.4.0")
+    // id 'com.matthewprenger.cursegradle' version '1.4.0' apply false
+    id("com.gtnewhorizons.retrofuturagradle").version("1.2.3")
+    // id 'com.gtnewhorizons.retrofuturagradle' version '1.2.3'
 }
